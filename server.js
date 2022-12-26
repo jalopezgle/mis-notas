dff5cc1f2f441e06dbc2479f3375511956215ab3
jalopezgle <jalopezgle@gmail.com>
2022-12-26 20:44:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 var cors = require('cors');
 const app = express();
 
+app.use(cors()); // <---- use cors middleware
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
