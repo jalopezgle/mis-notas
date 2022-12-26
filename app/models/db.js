c3c6eb2bb2510c050399ae91e5736d6e8567bed7
jalopezgle <jalopezgle@gmail.com>
2022-12-26 20:14:26 +0100

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,20 +1,7 @@
-// const mysql = require("pg");
-// const dbConfig = require("../config/db.config.js");
-
-// var connection = mysql.createPool({
-  // host: dbConfig.HOST,
-  // port: dbConfig.PORT,
-  // user: dbConfig.USER,
-  // password: dbConfig.PASSWORD,
-  // database: dbConfig.DB
-// });
-
-// module.exports = connection;
-
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: false
 });
 
 module.exports = pool;
