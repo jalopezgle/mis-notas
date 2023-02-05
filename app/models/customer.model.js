d0b0ba594e1b2b6d552f8e59ba4376b8c37d0c03
jalopezgle <jalopezgle@gmail.com>
2023-02-05 21:56:20 +0100

    Update customer.model.js

--- app/models/customer.model.js
+++ app/models/customer.model.js
@@ -70,6 +70,27 @@ Customer.findById = (customerId, result) => {
   });
 };
 
+Customer.findOne = (customerId, result) => {
+  sql.query("SELECT * FROM customers WHERE id = ${customerId}", (err, res) => {
+    if (err) {
+      console.log("error: ", err);
+      result(null, err);
+      return;
+    }
+
+    if (res.length) {
+      console.log("found customer: ", res[0]);
+      result(null, res[0]);
+      return;
+    }
+
+    // not found Customer with the id
+    result({ kind: "not_found" }, null);
+  });
+};
+
+
+
 Customer.getAll = result => {
   sql.query("SELECT id,email FROM customers where email <> 'mail' order by id desc", (err, res) => {
     if (err) {
